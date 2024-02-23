95005806af0225d7805881e5d2c3ee5623d41884
makov3c <makovecbarbara1@gmail.com>
2024-02-23 09:41:41 +0100

    hello world

--- predavanja/prvoPredavanje.c
+++ predavanja/prvoPredavanje.c
@@ -3,5 +3,21 @@
 // DOMAČE NALOGE
 // niso obvezne
 // IZPITI
-// 2 kolokvija (2 termina, 6 in 8)   ali     3 izpitni roki
+// 3 izpitni roki
+// kolokviji se mogoče(?) štejejo za opravljanje izpita. Dovolj dobro moraš pisati 1. da greš lahko na 2. 
 // (pišejo se na računalnik)
+// literatura ni dovoljena, c dokumentacij./a je na računalniku
+// splošni ljudski odpor in družbena samozaščita
+
+#include <stdio.h>
+
+
+int main() {
+    printf("Hello world! \n");
+    printf("test1");
+    
+}
+
+// c ni OOP zato nimamo classov, objektov
+// ./prvoPredavanje.c
+// ukaz v terminalu požene tisti program, ki smo ga nazadnje pognali, in ne tistega, ki ga imamo trenutno shranjennega
---
+++ predavanja/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("Hello world! \n");
+    
+    char a;
+    a = getchar();
+
+    printf("The entered char was: ");
+    putchar(a);
+    return 0;
+}
